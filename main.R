897d28003a942490141756b259b462e95def5081
iammabodo <eltone.mabodo@gmail.com>
2024-06-08 13:24:42 +0700

    Longer names now used

--- main.R
+++ main.R
@@ -3,7 +3,7 @@ library(here)
 
 # Load data
 
-tv_hrs_table <- gss_cat %>% 
+tv_hrs_tabulation <- gss_cat %>% 
   # Filter to only include those with age above 30
   filter(age < 30) %>%
   group_by(marital) %>%
@@ -11,4 +11,4 @@ tv_hrs_table <- gss_cat %>%
 
 
 # Save data as a .csv file
-write_csv(tv_hrs_table, here("tv_hrs_table.csv"))
+write_csv(tv_hrs_tabulation, here("tv_hrs_table.csv"))
